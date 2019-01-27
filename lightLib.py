1854953b457a4760ab5354400d05da4c85f442e0
BaileyFoot <email2baileyf@googlemail.com>
2019-01-27 22:06:29 +0000

    Add files via upload

---
+++ lightLib.py
@@ -0,0 +1,93 @@
+#Import stuff
+import RPi as GPIO
+import time
+import sys
+
+
+class Pi:
+
+    def __init__(self, mode='bcm'):
+
+        self.mode = mode
+        #self.setupGPIO(mode)
+
+
+    def setupGPIO(self):
+
+        #sets board GPIO numbering to BCM or BOARD based on input.
+        if self.mode == 'bcm':
+            GPIO.setmode(GPIO.BCM)
+        elif self.mode == 'board':
+            GPIO.setmode(GPIO.BOARD)
+        else:
+            print(
+            '''
+            This is not a vaild option...
+            *exiting*
+            '''
+            )
+            sys.exit()
+
+
+    def cleanup(self):
+        print(
+        '''
+        *GPIOcleanup*
+        Pins will be set to Low...
+        '''
+        )
+        GPIO.cleanup()
+
+
+class Light:
+
+    def __init__(self, pi, pins):
+        #creates red, green and blue pins properties.
+        self.pi = pi
+        self.pins = []
+        self.redPin = pins[0]
+        self.greenPin = pins[1]
+        self.bluePin = pins[2]
+
+        #creates desired red, green and blue colour val properties.
+        #dont't think I need this.
+        #self.redVal = redVal
+        #self.greenVal = greenVal
+        #self.blueVal = blueVal
+
+    def setupPins(self):
+        #sets up red, green and blue pins for output.
+        GPIO.setup(self.redPin, GPIO.OUT)
+        GPIO.setup(self.greenPin, GPIO.OUT)
+        GPIO.setup(self.bluePin, GPIO.OUT)
+
+
+
+    def changeColour(redVal, greenVal, blueVal):
+        red = GPIO.PWM(self.redPin, 50)
+        green = GPIO.PWM(self.greenPin, 50)
+        blue = GPIO.PWM(self.blue, 50)
+
+        red.start(0)
+        green.start(0)
+        blue.start(0)
+
+        red.ChangeDutyCycle(redVal)
+        green.ChangeDutyCycle(greenVal)
+        blue.ChangeDutyCycle(blueVal)
+        #https://raspi.tv/2013/how-to-use-soft-pwm-in-rpi-gpio-pt-2-led-dimming-and-motor-speed-control
+        #https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+
+
+        #Could just take one value as input. format: rrr,ggg,bbb.
+        #this would be readable from text file
+
+        red = GPIO.PWM() #need to put GPIO pin num in brackets. SCOPE!
+        green = GPIO.PWM()
+        blue = GPIO.PWM()
+        red.start(0)
+        green.start(0)
+        blue.start(0)
+
+#will all be in main.py...
+#This is only for testing.
---
+++ main.py
@@ -0,0 +1,19 @@
+import lightLib
+#need ,lightControls in front of anything that belongs in other file...
+pi = lightLib.Pi()
+pi.setupGPIO()
+#creating instance of Pi and setting up gpio lib.
+
+lightOne = lightLib.Light(pi,[22,23,24])
+lightOne.setupPins()
+#creating instance of Light and running pin setup.
+
+while True:
+    try:
+        red = int(input("red"))
+        green = int(input("green"))
+        blue = int(input("blue"))
+
+        lightOne.changeColour(red, green, blue)
+    except:
+        pi.cleanup()
