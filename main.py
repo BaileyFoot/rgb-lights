92bcf60d09f0adf5506ee74259b436202f1e71a3
BaileyFoot <email2baileyf@googlemail.com>
2019-02-05 22:25:37 +0000

    Commented calling of change colour function in main.py called test function in main.py for debugging

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ lightOne = lightLib.Light(pi,[22,23,24])
 #lightOne.setupPins()
 #creating instance of Light and running pin setup.
 
-red = int(input("red"))
-green = int(input("green"))
-blue = int(input("blue"))
+lightOne.test()
 
-lightOne.changeColour(red, green, blue)
+#red = int(input("red"))
+#green = int(input("green"))
+#blue = int(input("blue"))
+
+#lightOne.changeColour(red, green, blue)
