c876a32f3cf1b97dbba4cf51e87eeeda3cf4f1cf
BaileyFoot <email2baileyf@googlemail.com>
2019-05-14 15:00:44 +0100

    changed munbering mode in display.py to bcm

--- display.py
+++ display.py
@@ -1,7 +1,7 @@
 from RPLCD import CharLCD
 import time
 
-lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33,31,29,23])
+lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33,31,29,23], numbering_mode=GPIO.BCM)
 
 def output(message):
     lcd.write_string(message)
