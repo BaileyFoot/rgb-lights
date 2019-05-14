ee4ab111d7e896700bb0f1b111e0566763891977
BaileyFoot <email2baileyf@googlemail.com>
2019-05-14 15:03:50 +0100

    reversing previous changes, importing gpio into display.py

--- display.py
+++ display.py
@@ -1,4 +1,5 @@
-from RPLCD.gpio import CharLCD
+from RPLCD import CharLCD
+import RPi.GPIO as GPIO
 import time
 
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33,31,29,23], numbering_mode=GPIO.BCM)
