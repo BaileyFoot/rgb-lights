f834981d79bd2909da25a4bfa09cf951cbe6842e
BaileyFoot <email2baileyf@googlemail.com>
2019-05-14 10:00:34 +0100

    Making changes to how array of Components is defined

--- lightLib.py
+++ lightLib.py
@@ -44,6 +44,7 @@ class Component:
 
         self.pi = pi
         #runs through array of pins, creating them as properties of component.
+        self.pins = []
         for i in range(len(pins)):
             self.pins[i] = pins[i]
 
