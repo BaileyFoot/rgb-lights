c7cc795d9206700ff20cdf3ac984fe26ddc3d73b
BaileyFoot <email2baileyf@googlemail.com>
2019-05-15 15:03:53 +0100

    changing pin numbers in main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pi = lightLib.Pi()
 pi.setupGPIO()
 #creating instance of Pi and setting up gpio lib.
 
-lightOne = lightLib.Light(pi,[26,24,23])
+lightOne = lightLib.Light(pi,[7,9,11]) #26,21,23
 #lightOne.setupPins()
 #creating instance of Light and running pin setup.
 
