7cc41863feedd87ea42c2330ce71b6d99812e48b
BaileyFoot <email2baileyf@googlemail.com>
2019-05-16 17:05:40 +0100

    created instance of Button class

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ pi.setupGPIO()
 lightOne = lightLib.Light(pi,[11,13,15]) #26,21,23
 #creating instance of Light.
 
+ButtonOne = lightLib.Button(pi,12)
+#creating instance of Button.
+
 #lightOne.testOutputs()
 
-lightOne.cycleColours() 
+lightOne.cycleColours()
 
 input()
