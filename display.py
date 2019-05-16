35f9b85c5c2aa592a57903b7de500009f0c49ddf
BaileyFoot <email2baileyf@googlemail.com>
2019-05-16 23:39:05 +0100

    debugging

--- display.py
+++ display.py
@@ -5,6 +5,5 @@ import time
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33,31,29,23])
 
 def output(message):
-    #lcd.write_string(message)
-    lcd.write(message)
+    lcd.write_string(message)
     print(message)
